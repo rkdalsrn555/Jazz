66ebf9a8078af95d9e7feb5c1e51591b56a268c3
ghwhdthd@naver.com <ghwhdthd@naver.com>
2023-09-13 17:03:52 +0900

    기본 프로젝트

---
+++ jazz_backend/src/main/java/com/ssafy/jazz_backend/JazzBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.jazz_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazzBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazzBackendApplication.class, args);
+	}
+
+}
---
+++ jazz_backend/src/test/java/com/ssafy/jazz_backend/JazzBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.jazz_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazzBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

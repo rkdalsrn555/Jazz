aacb1628a3ca20176f0ffa752b38f41f5dc18f76
ghwhdthd@naver.com <ghwhdthd@naver.com>
2023-09-20 18:01:01 +0900

    S09P22A708-519 Feat: 레벨 랭킹 컨트롤러 구현 중

--- jazz_backend/src/main/java/com/ssafy/jazz_backend/domain/member/record/controller/RankingController.java
+++ jazz_backend/src/main/java/com/ssafy/jazz_backend/domain/member/record/controller/RankingController.java
@@ -1,20 +1,31 @@
 package com.ssafy.jazz_backend.domain.member.record.controller;
 
 
+import com.ssafy.jazz_backend.domain.jwt.service.JwtService;
 import com.ssafy.jazz_backend.domain.member.record.dto.LevelRankingResponseDto;
+import com.ssafy.jazz_backend.domain.member.record.service.RankingService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ranking")
+@RequiredArgsConstructor
 public class RankingController {
+
+    private final RankingService rankingService;
+
 
     //update가 일어나면 바로 redis에 값 넣고 db에도 바꿔줌
     @GetMapping("/level")
-    private ResponseEntity<LevelRankingResponseDto> getLevelRankingTopTen() {
-
+    private ResponseEntity<LevelRankingResponseDto> getLevelRankingTopTen(
+        @RequestHeader String accessTocken) {
+        List<LevelRankingResponseDto> levelRankingTopTen = rankingService.getLevelRankingTopTen(
+            accessTocken);
         return null;
     }
 

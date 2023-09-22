b156fe5f14dcb0ad1a49d22b6884c041bb3b1cc1
rkdalsrn314 <rkdalsrn314@gmail.com>
2023-09-22 09:19:48 +0900

    S09P22A708-417 Fix: WebSocket DTO 수정
    - WebSocket 도메인 DTO Getter, Setter 수정하였습니다.
    - 직렬화를 위한 기본 생성자 추가하였습니다.

--- jazz_backend/src/main/java/com/ssafy/jazz_backend/domain/websocket/dto/GameMyInfo.java
+++ jazz_backend/src/main/java/com/ssafy/jazz_backend/domain/websocket/dto/GameMyInfo.java
@@ -2,10 +2,12 @@ package com.ssafy.jazz_backend.domain.websocket.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class GameMyInfo {
     private String userUUID;
--- jazz_backend/src/main/java/com/ssafy/jazz_backend/domain/websocket/dto/MyInfo.java
+++ jazz_backend/src/main/java/com/ssafy/jazz_backend/domain/websocket/dto/MyInfo.java
@@ -1,7 +1,13 @@
 package com.ssafy.jazz_backend.domain.websocket.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class MyInfo {
     private String userId;

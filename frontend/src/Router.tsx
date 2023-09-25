689ff5aee826bea0441be2ceea14a9ce76cd4af8
CHAERYEON-LEE <sogogijjang@naver.com>
2023-09-25 22:23:30 +0900

    S09P22A708-565 Feat: 로그인이 필요한 서비스 라우팅처리 완료
    - PrivateRoute를 이용한 라우팅 처리

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -7,20 +7,34 @@ import ShortAnswerQuestionPage from 'pages/Quiz/ShortAnswerQuestionPage';
 import Login from 'pages/Login/Login';
 import Signup from 'pages/Signup/Signup';
 import Result from 'pages/Quiz/Result';
+import PrivateRoute from 'hooks/PrivateRoute';
+
+const route = [
+  { path: '/', component: <Home /> },
+  { path: '/home', component: <Home /> },
+  { path: '/battle-game', component: <BattleGame /> },
+  { path: '/companySearch', component: <CompanySearch /> },
+  { path: '/statement/:companyId', component: <Statement /> },
+  { path: '/short-answer-question', component: <ShortAnswerQuestionPage /> },
+  { path: '/result', component: <Result /> },
+];
 
 const Router = () => {
+  const userAccessToken = window.localStorage.getItem('userAccessToken');
+
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/battle-game" element={<BattleGame />} />
-      <Route path="/companySearch" element={<CompanySearch />} />
-      <Route path="/statement/:companyId" element={<Statement />} />
-      <Route
-        path="/short-answer-question"
-        element={<ShortAnswerQuestionPage />}
-      />
-      <Route path="/result" element={<Result />} />
+      {route.map((item) => (
+        <Route
+          path={item.path}
+          element={
+            <PrivateRoute
+              component={item.component}
+              authenticated={userAccessToken}
+            />
+          }
+        />
+      ))}
       {/* 로그인, 회원가입 */}
       <Route path="/login" element={<Login />} />
       <Route path="/sign-up" element={<Signup />} />

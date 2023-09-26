61f6be4866c9b54852f0c35b55edc3356b977ef7
jongho-song <ghwhdthd@naver.com>
2023-09-26 17:35:09 +0900

    S09P22A708-647 Feat: 마라톤 월간 랭킹 구현 중

--- jazz_backend/src/main/java/com/ssafy/jazz_backend/domain/member/record/entity/Season.java
+++ jazz_backend/src/main/java/com/ssafy/jazz_backend/domain/member/record/entity/Season.java
@@ -3,11 +3,8 @@ package com.ssafy.jazz_backend.domain.member.record.entity;
 import jakarta.persistence.Entity;
 
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Builder.Default;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 

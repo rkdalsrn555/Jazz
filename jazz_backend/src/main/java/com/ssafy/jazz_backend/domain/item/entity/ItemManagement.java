58f17e4a0d3e1d541b1e9567fbaca86735ef53a6
rkdalsrn314 <rkdalsrn314@gmail.com>
2023-09-27 09:45:58 +0900

    S09P22A708-665 Feat: 상점 기능 개발
    - 상점 기능을 위한 Item 관련 Entity 패키지 생성하였습니다.

--- jazz_backend/src/main/java/com/ssafy/jazz_backend/domain/item/entity/ItemManagement.java
+++ jazz_backend/src/main/java/com/ssafy/jazz_backend/domain/item/entity/ItemManagement.java
@@ -1,4 +1,49 @@
 package com.ssafy.jazz_backend.domain.item.entity;
 
+import com.ssafy.jazz_backend.domain.member.entity.Member;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "itemmanagement")
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class ItemManagement {
+
+    @EmbeddedId
+    private ItemManagementId id;
+
+    @Builder.Default
+    private boolean isOwn = false;
+
+    @Builder.Default
+    private boolean isUsed = false;
+
+    @MapsId("member")
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    public ItemManagement(Member member, Item item, boolean isOwn, boolean isUsed) {
+        this.id = new ItemManagementId(member, item);
+        this.isOwn = isOwn;
+        this.isUsed = isUsed;
+    }
+
 }

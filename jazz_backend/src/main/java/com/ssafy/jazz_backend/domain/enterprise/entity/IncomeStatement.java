7cbb72566fb66e326be69a33a79057affc204fd0
ping9oo <fpdl96@gmail.com>
2023-09-27 15:56:26 +0900

    S09P22A708-677 Feat: JSON 파싱을 위한 엔터티 구성(미완)

--- jazz_backend/src/main/java/com/ssafy/jazz_backend/domain/enterprise/entity/IncomeStatement.java
+++ jazz_backend/src/main/java/com/ssafy/jazz_backend/domain/enterprise/entity/IncomeStatement.java
@@ -1,4 +1,46 @@
 package com.ssafy.jazz_backend.domain.enterprise.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "IncomeStatement")
 public class IncomeStatement {
+    @EmbeddedId
+    private IncomeStatementId id;
+
+    @MapsId("enterpriseId") // IncomeStatementId의 enterpriseId와 매핑
+    @ManyToOne
+    @JoinColumn(name = "enterprise_id", referencedColumnName = "id", insertable = false, updatable = false)
+    private Enterprise enterprise;
+
+    @Column(name = "account_name", length = 100)
+    private String accountName;
+
+    @Column(name = "thstrm_name", length = 100)
+    private String thstrmName;
+
+    @Column(name = "th_amount")
+    private Long thAmount;
+
+    @Column(name = "frmtrm_name", length = 100)
+    private String frmtrmName;
+
+    @Column(name = "frm_amount")
+    private Long frmAmount;
 }

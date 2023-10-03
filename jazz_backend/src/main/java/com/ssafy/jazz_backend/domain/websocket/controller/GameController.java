bc7d82d4f8804673ecf3aaed8ca80eb6eb2becb5
rkdalsrn314 <rkdalsrn314@gmail.com>
2023-10-04 01:08:14 +0900

    S09P22A708-417 Feat: 금융 배틀 게임 종료 API 작성
    - 금융 배틀 게임 종료 API 작성 완료했습니다.

--- jazz_backend/src/main/java/com/ssafy/jazz_backend/domain/websocket/controller/GameController.java
+++ jazz_backend/src/main/java/com/ssafy/jazz_backend/domain/websocket/controller/GameController.java
@@ -37,6 +37,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
@@ -102,8 +103,8 @@ public class GameController {
 
     @PatchMapping("/result")
     @ResponseBody
-    public GameResultResponse endGame(@RequestHeader("accessToken") String accessToken) {
-        GameResultResponse gameResultResponse = gameService.gameResult(accessToken);
+    public GameResultResponse endGame(@RequestHeader("accessToken") String accessToken, @RequestParam int lives) {
+        GameResultResponse gameResultResponse = gameService.gameResult(accessToken, lives);
 
         return gameResultResponse;
     }
--- jazz_backend/src/main/java/com/ssafy/jazz_backend/domain/websocket/service/GameService.java
+++ jazz_backend/src/main/java/com/ssafy/jazz_backend/domain/websocket/service/GameService.java
@@ -3,6 +3,13 @@ package com.ssafy.jazz_backend.domain.websocket.service;
 import com.ssafy.jazz_backend.domain.item.repository.ItemManagementJpaRepository;
 import com.ssafy.jazz_backend.domain.jwt.service.JwtService;
 import com.ssafy.jazz_backend.domain.member.entity.Member;
+import com.ssafy.jazz_backend.domain.member.profile.entity.Profile;
+import com.ssafy.jazz_backend.domain.member.profile.repository.ProfileRepository;
+import com.ssafy.jazz_backend.domain.member.record.entity.Season;
+import com.ssafy.jazz_backend.domain.member.record.entity.Tier;
+import com.ssafy.jazz_backend.domain.member.record.entity.TierId;
+import com.ssafy.jazz_backend.domain.member.record.repository.SeasonJpaRepository;
+import com.ssafy.jazz_backend.domain.member.record.repository.TierJpaRepository;
 import com.ssafy.jazz_backend.domain.member.repository.MemberRepository;
 import com.ssafy.jazz_backend.domain.quiz.repository.QuizRepository;
 import com.ssafy.jazz_backend.domain.websocket.dto.GameInitResponse;
@@ -56,6 +63,15 @@ public class GameService {
 
     @Autowired
     private QuizRepository quizRepository;
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    @Autowired
+    private SeasonJpaRepository seasonJpaRepository;
+
+    @Autowired
+    private TierJpaRepository tierJpaRepository;
 
     @Autowired
     private JwtService jwtService;
@@ -145,7 +161,7 @@ public class GameService {
             UserInfo user2Info = new UserInfo(member2.getProfile().getNickname(), member2.getProfile().getExpPoint() / 10 + 1, itemManagementJpaRepository.findItemIdByMemberIdAndIsUsed(member2.getId()).orElseThrow(() -> new NullPointerException()));
             GameUserInfo gameUser1Info = new GameUserInfo(user1.getSession(), 5, false);
             GameUserInfo gameUser2Info = new GameUserInfo(user2.getSession(), 5, false);
-            GameMessage gameMessage = new GameMessage(user1.getSession(), "Game Init Message", MessageType.GAME, 1, gameUser1Info, gameUser2Info);
+            GameMessage gameMessage = new GameMessage(user1.getSession(), "Game Init Message", MessageType.GAME, "", 1, gameUser1Info, gameUser2Info);
 
             user1Result.setResult(
                 new GameResponse(ResponseResult.SUCCESS, uuid, user1.getSession(), user2.getMemberUUID(), user1Info, user2Info, gameMessage));
@@ -210,11 +226,43 @@ public class GameService {
         return null;
     }
 
-    public GameResultResponse gameResult(String accessToken) {
-        /*
-        프론트와 협의 후 작성
-         */
+    public GameResultResponse gameResult(String accessToken, int lives) {
+        String memberId = jwtService.getInfo("account", accessToken);
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new NullPointerException());
+        Profile profile = profileRepository.findById(memberId).orElseThrow(() -> new NullPointerException());
+        int diamond = profile.getDiamond();
+        profile.setDiamond(diamond + 10);
+
+        Season season = seasonJpaRepository.findById(1).orElseThrow(() -> new NullPointerException());
+        TierId tierId = new TierId(member, season.getTierSeason());
+        Tier tier = tierJpaRepository.findById(tierId).orElseThrow(() -> new NullPointerException());
+
         GameResultResponse gameResultResponse = new GameResultResponse();
+        int temp = 0;
+        if(lives <= 0) {
+            temp = profile.getLose();
+            profile.setLose(temp + 1);
+            tier.setLose(temp + 1);
+            tier.setRankPoint(tier.getRankPoint() - 5);
+            gameResultResponse.setResult(3);
+        }
+        else if(lives == 99) {
+            temp = profile.getDraw();
+            profile.setDraw(temp + 1);
+            tier.setDraw(temp + 1);
+            gameResultResponse.setResult(2);
+        }
+        else {
+            temp = profile.getWin();
+            profile.setWin(temp + 1);
+            tier.setWin(temp + 1);
+            tier.setRankPoint(tier.getRankPoint() + 10);
+            gameResultResponse.setResult(1);
+        }
+
+        profileRepository.save(profile);
+        tierJpaRepository.save(tier);
+
         return gameResultResponse;
     }
 

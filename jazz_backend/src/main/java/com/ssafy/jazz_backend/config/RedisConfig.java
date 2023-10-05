1054c8fdd4abb10ba9f7e69d2c8b336a73caf789
ghwhdthd@naver.com <ghwhdthd@naver.com>
2023-10-05 17:02:31 +0900

    Docs: redis Config 수정

--- jazz_backend/src/main/java/com/ssafy/jazz_backend/config/RedisConfig.java
+++ jazz_backend/src/main/java/com/ssafy/jazz_backend/config/RedisConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
@@ -18,9 +20,16 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private Integer port;
 
+    @Value("${spring.redis.password}") // 추가: application.properties에서 비밀번호 읽기
+    private String password;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(host, port);
+        configuration.setPassword(RedisPassword.of(password)); // 패스워드 설정
+
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(configuration);
+        return lettuceConnectionFactory;
     }
 
     @Bean

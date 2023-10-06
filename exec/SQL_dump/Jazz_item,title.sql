fa0cb2692d560217f6d69625f252aee26def5290
송종호 <ghwhdthd@naver.com>
2023-10-06 11:20:22 +0900

    sql 덤프파일 업로드

---
+++ exec/SQL_dump/Jazz_item,title.sql
@@ -0,0 +1,40 @@
+USE jazzdb;
+# Item Entity
+INSERT INTO item(price, kind) VALUES(0, 3);
+INSERT INTO item(price, kind) VALUES(50, 3);
+INSERT INTO item(price, kind) VALUES(50, 3);
+INSERT INTO item(price, kind) VALUES(50, 3);
+INSERT INTO item(price, kind) VALUES(50, 3);
+
+# PreTitle Entity
+INSERT INTO pre_title(degree, content) VALUES
+	(1, "가난한"),
+	(1, "배고픈"),
+	(1, "부유한"),
+	(1, "섹시한"),
+	(1, "아름다운"),
+	(1, "당당한"),
+	(1, "즐거운"),
+	(1, "전설의"),
+	(1, "불량한"),
+	(1, "트렌디한"),
+	(1, "금빛"),
+	(1, "국민");
+
+# SuffixTitle Entity
+INSERT INTO suffix_title(degree, content) VALUES
+	(1, "초보"),
+	(1, "부자"),
+	(1, "거지"),
+	(1, "챔피언"),
+	(1, "비둘기"),
+	(1, "엉덩이"),
+	(1, "욕심쟁이"),
+	(1, "심장"),
+	(1, "사장님"),
+	(1, "1등"),
+	(1, "전문가"),
+	(1, "병아리"),
+	(1, "젊은이"),
+	(1, "어린이");
+	
